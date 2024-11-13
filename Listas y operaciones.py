4657cddec71f7ca3215a37a6ca3c34ad08988fa2
Jonathan25OC <ordonezjonathan404@gmail.com>
2024-11-13 14:48:41 -0600

    Add files via upload

---
+++ Listas y operaciones.py
@@ -0,0 +1,16 @@
+# Crear una lista
+numeros = [1, 2, 3, 4, 5]
+
+# Agregar un elemento
+numeros.append(6)
+
+# Eliminar un elemento
+numeros.remove(3)
+
+# Acceder a un elemento por índice
+print(numeros[0])  # Primer elemento
+print(numeros[-1])  # Último elemento
+
+# Recorrer la lista
+for numero in numeros:
+    print(numero)
